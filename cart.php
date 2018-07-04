17064d3e042056f53d12592791f1b6fd593797e2
Parinya Yodming <parinya@192.168.1.48>
2018-07-04 13:52:35 +0700

    update
    uppdate (pond)

---
+++ cart.php
@@ -0,0 +1,262 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<!-- START HEADER -->
+    <?php include_once('inc/header.php'); ?>
+    <!-- END HEADER -->
+  </head>
+
+  <body>
+  	<!-- START NAVBAR -->
+    <?php include_once('inc/navbar.php'); ?>
+    <!-- END NAVBAR -->
+
+
+
+    <!-- START CONTENT -->
+      <div class="wrapper fadeIn animated">
+        <!-- CART SIDEBAR -->
+        <?php include_once('inc/cart-sidebar.php'); ?>
+        <?php include_once('inc/nav-sidebar.php'); ?>
+        <!-- END CART SIDEBAR -->
+
+
+        <!-- SITE CONTENT -->
+        <div class="container-fluid custom-container">
+            <div class="row p-2">
+               <div class="col-xl-9 mt-5">
+                   <div class="cart-font2">MY CART</div>
+                   <div class="row pt-3">
+                       <div class="col-xl-2 col-lg-2 col-md-3 col-12">
+                          <img src="/dilok/assets/images/productfill/1.png" class="cart-size">
+                       </div>
+                       <div class="col-xl-3 col-lg-4 col-md-6 col-12 cart-p-l">
+                           <div class="pay-font8 cart-m-t2">Pureboost DPR Shoes</div>
+                           <div class="pay-font9">Men running</div>
+                           <div class="pay-font9 mt-2">Color: White-Blue</div>
+                           <div class="pay-font9">Size: [UK] 7.5</div>
+                           <div class="sale-percent mt-2 cart-m-l5">
+                              <span class="">ลด 50%</span>
+                           </div>
+                       </div>
+                       <div class="col-xl-6 col-lg-6 cart-bg latest-product-price">
+                           <div class="row">
+                               <div class="col-xl-4 col-lg-4 col-md-4 col-12 text-center">
+                                   <span class="before">1,825</span>
+                                   <span class="after">925</span>
+                                   <span class="currency">THB</span>
+                               </div>
+                               <div class="col-xl-4 col-lg-4 col-md-4 col-12 text-center cart-m-t3">
+                                   <span class="">
+                                       <select class="cart-selected">
+                                          <option value="0">1</option>
+                                          <option value="1">2</option>
+                                          <option value="2">2</option>
+                                       </select>
+                                   </span>
+                               </div>
+                               <div class="col-xl-4 col-lg-4 col-md-4 col-12 text-center cart-m-t3">
+                                  <span class="pay-m-t cart-font3">1,850 THB</span>
+                               </div>
+                           </div>
+                       </div>
+                    </div>
+                    <div class="row mt-2">
+                         <div class="col-xl-2"></div>
+                         <div class="col-xl-10">
+                             <div class="row">
+                                 <div class="col-xl-6 col-lg-6 col-md-6 col-12 text-center text-lg-left  text-md-left">
+                                     <span>
+                                       <a href="#" class="cart-font4">
+                                         <i class="fas fa-pen"></i> Edit
+                                       </a>
+                                     </span>
+                                     <span class="cart-m-l">
+                                       <a href="#" class="cart-font4">
+                                          <i class="fas fa-times pr-2"></i>Delete
+                                       </a>
+                                     </span>
+                                     <span class="cart-m-l">
+                                       <a href="#" class="cart-font4">
+                                          <i class="far fa-heart"></i> Add to Wishlist
+                                       </a>
+                                     </span>
+                                 </div>
+                                 <div class="col-xl-6 col-lg-6 col-md-6 col-12 text-center text-lg-left text-md-left">
+                                     <span class="cart-m-l2">
+                                       <a href="#" class="cart-font4">
+                                         <i class="fas fa-truck"></i> จัดส่งสินค้าระหว่าง 07/25/2561 - 15/06/2561
+                                       </a>
+                                     </span>
+                                 </div>
+                              </div>
+                         </div>
+                     </div>
+                     <hr>
+                     <div class="row pt-3">
+                         <div class="col-xl-2 col-lg-2 col-md-3 col-12">
+                            <img src="/dilok/assets/images/productfill/1.png" class="cart-size">
+                         </div>
+                         <div class="col-xl-3 col-lg-4 col-md-6 col-12 cart-p-l">
+                             <div class="pay-font8 cart-m-t2">Pureboost DPR Shoes</div>
+                             <div class="pay-font9">Men running</div>
+                             <div class="pay-font9 mt-2">Color: White-Blue</div>
+                             <div class="pay-font9">Size: [UK] 7.5</div>
+                             <div class="sale-percent mt-2 cart-m-l5">
+                                <span class="">ลด 50%</span>
+                             </div>
+                         </div>
+                         <div class="col-xl-6 col-lg-6 cart-bg latest-product-price">
+                             <div class="row">
+                                 <div class="col-xl-4 col-lg-4 col-md-4 col-12 text-center">
+                                     <span class="before">1,825</span>
+                                     <span class="after">925</span>
+                                     <span class="currency">THB</span>
+                                 </div>
+                                 <div class="col-xl-4 col-lg-4 col-md-4 col-12 text-center cart-m-t3">
+                                     <span class="">
+                                         <select class="cart-selected">
+                                            <option value="0">1</option>
+                                            <option value="1">2</option>
+                                            <option value="2">2</option>
+                                         </select>
+                                     </span>
+                                 </div>
+                                 <div class="col-xl-4 col-lg-4 col-md-4 col-12 text-center cart-m-t3">
+                                    <span class="pay-m-t cart-font3">1,850 THB</span>
+                                 </div>
+                             </div>
+                         </div>
+                      </div>
+                      <div class="row mt-2">
+                           <div class="col-xl-2"></div>
+                           <div class="col-xl-10">
+                               <div class="row">
+                                   <div class="col-xl-6 col-lg-6 col-md-6 col-12 text-center text-lg-left  text-md-left">
+                                       <span>
+                                         <a href="#" class="cart-font4">
+                                           <i class="fas fa-pen"></i> Edit
+                                         </a>
+                                       </span>
+                                       <span class="cart-m-l">
+                                         <a href="#" class="cart-font4">
+                                            <i class="fas fa-times pr-2"></i>Delete
+                                         </a>
+                                       </span>
+                                       <span class="cart-m-l">
+                                         <a href="#" class="cart-font4">
+                                            <i class="far fa-heart"></i> Add to Wishlist
+                                         </a>
+                                       </span>
+                                   </div>
+                                   <div class="col-xl-6 col-lg-6 col-md-6 col-12 text-center text-lg-left text-md-left">
+                                       <span class="cart-m-l2">
+                                         <a href="#" class="cart-font4">
+                                           <i class="fas fa-truck"></i> จัดส่งสินค้าระหว่าง 07/25/2561 - 15/06/2561
+                                         </a>
+                                       </span>
+                                   </div>
+                                </div>
+                           </div>
+                       </div>
+                    <hr>
+               </div>
+
+               <div class="col-xl-3  mt-5">
+                 <div class="card pay-b-n">
+                   <div class="row">
+                       <div class="col-xl-12">
+                           <div class="row mt-3 pay-padding">
+                               <div class="col-xl-8 col-lg-6 col-md-6 col-12 text-center text-lg-left text-md-left pay-m-l-r">
+                                    <div class="pay-font1">ORDER SUMARY</div>
+                               </div>
+                               <div class="col-xl-4 col-lg-6  col-md-6 col-12 text-center text-lg-right text-md-right pay-m-l-r">
+                                    <div class="pay-font7" >4 items</div>
+                               </div>
+                           </div>
+                           <div class="row mt-4 pay-padding">
+                               <div class="col-xl-6 col-lg-6 col-md-6 col-6 pay-m-l-r">
+                                   <div class="pay-font10">Subtotal</div>
+                                   <div class="pay-font10 mt-1">Delivery</div>
+                               </div>
+                               <div class="col-xl-6 col-lg-6 col-md-6 col-6 pay-m-l-r">
+                                   <div class="pay-font10 text-right">3,700 THB</div>
+                                   <div class="pay-font10 text-right mt-1">0 THB</div>
+                               </div>
+                           </div>
+                           <div class="row mt-4 pay-padding">
+                               <div class="col-xl-12">
+                                   <div class="pay-font10">
+                                     <input type="text" class="cart-form" placeholder="Promo Code">
+                                   </div>
+                                   <div class="cart-font1 mt-2">Promotion code description here.</div>
+                               </div>
+                           </div>
+                           <div class="row mt-3 bg-gray py-3 pay-m-r-l2">
+                               <div class="col-xl-6 col-lg-6  col-md-6 col-6 pay-m-l-r">
+                                   <div class="pay-font10">Total</div>
+                               </div>
+                               <div class="col-xl-6 col-lg-6 col-md-6 col-6 pay-m-l-r text-right">
+                                   <div class="pay-font11" style="display:inline;">3,700</div>
+                                   <div class="pay-font10" style="display:inline;"> THB</div>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+                 </div>
+                 <div class="row mt-4">
+                     <div class="col-xl-5 col-lg-4 col-md-4"></div>
+                     <div class="col-xl-7 col-lg-4 col-md-4 pl-3 pr-3">
+                         <div class="checkout">
+                           <button class="btn checkout-btn">
+                             <span class="pull-left">Order out</span>
+                             <span class="pull-right">></span>
+                           </button>
+                         </div>
+                     </div>
+                     <div class="col-xl-12 mt-2">
+                         <hr>
+                           <a href="#">
+                               <div class="promotion-button">
+                                  <span class="pull-left">นโยบายการคือสินค้า</span>
+                                  <span class="fa fa-plus pull-right"></span>
+                               </div>
+                           </a>
+                         <hr class="mt-0">
+                           <a href="#">
+                               <div class="promotion-button">
+                                  <span class="pull-left">ช่องทางการชำระสินค้า</span>
+                                  <span class="fa fa-plus pull-right"></span>
+                               </div>
+                           </a>
+                         <hr class="mt-0">
+                     </div>
+                 </div>
+               </div>
+            </div>
+
+            <section class="structure py-5">
+              <?php include_once('inc/latest-product.php'); ?>
+            </section>
+        </div>
+        <!-- END SITE CONTENT -->
+      </div>
+    <!-- END CONTENT -->
+
+    <!-- START FOOTER -->
+    <?php include_once('inc/footer.php'); ?>
+    <!-- END FOOTER -->
+
+    <!-- START FOOTER SCRIPT -->
+    <?php include_once('inc/footer-script.php'); ?>
+    <!-- END FOOOTER SCRIPT -->
+
+
+  </body>
+</html>
+
+
+<script>
+
+
+</script>
---
+++ forgot.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- START HEADER -->
+  <?php include_once('inc/header.php'); ?>
+  <!-- END HEADER -->
+  <!-- START NAVBAR -->
+  <?php include_once('inc/navbar.php'); ?>
+  <!-- END NAVBAR -->
+</head>
+  <body>
+    <div class="wrapper">
+    <div class="container pb-5 fadeIn animated">
+      <!-- CART SIDEBAR -->
+      <?php include_once('inc/cart-sidebar.php'); ?>
+      <?php include_once('inc/nav-sidebar.php'); ?>
+      <!-- END CART SIDEBAR -->
+        <div class="col-12 text-center mt-5">
+            <div class="forgot-font1">FORGOT YOUR PASSWORD?</div>
+        </div>
+        <div class="col-12 text-center mt-4">
+            <div class="regist-font3">Enter your email below and we'll send you a link to reset your password.</div>
+        </div>
+        <div class="row forgot-m-t">
+            <div class="col-12">
+              <form>
+                 <div class="form-group">
+                      <label class="forgot-font2 forgot-m-l d-flex">
+                          Email Address
+                          <span class="forgot-font3">*</span>
+                          <span class="forgot-font4 forgot-m-l2">*Required fields</span>
+                      </label>
+                      <div class="text-center">
+                         <input type="text" name="" value="" class="forgot-form" placeholder="Email">
+                      </div>
+                 </div>
+              </form>
+            </div>
+        </div>
+        <div class="row forgot-m-t2">
+            <div class="col-12 text-center">
+               <button type="button" class="btn forgot-btn">SUBMIT</button>
+            </div>
+        </div>
+    </div>
+  </div>
+    <!-- START FOOTER -->
+    <?php include_once('inc/footer.php'); ?>
+    <!-- END FOOTER -->
+
+    <!-- START FOOTER SCRIPT -->
+    <?php include_once('inc/footer-script.php'); ?>
+    <!-- END FOOOTER SCRIPT -->
+  </body>
+</html>
