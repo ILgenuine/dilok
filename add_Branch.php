b4ca8ed15b76bb0000229936aa4fbd60a2e24d78
gggia <gggia36@gmail.com>
2018-07-04 10:12:43 +0700

    up by gia.css launches.php add_Branch.php
    4/7/61
    10.12

---
+++ add_Branch.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<!-- START HEADER -->
+    <?php include_once('inc/header.php'); ?>
+    <!-- END HEADER -->
+  </head>
+
+  <body>
+	<!-- START NAVBAR -->
+    <?php include_once('inc/navbar.php'); ?>
+    <!-- END NAVBAR -->
+
+
+    <!-- START CONTENT -->
+      <div class="wrapper">
+
+        <!-- CART SIDEBAR -->
+        <?php include_once('inc/cart-sidebar.php'); ?>
+        <?php include_once('inc/nav-sidebar.php'); ?>
+        <!-- END CART SIDEBAR -->
+
+
+        <!-- SITE CONTENT -->
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12 text-center mt-4">
+              <h2>OUR STORES</h2> 
+            </div>
+            <div class="col-lg-12 text-center ">
+              <p>END storse simply dummy text of the printing and typesetting industry. Lorem </p>  
+            </div>
+            <div class="col-lg-6 mb-4">
+              <div class="Add_Branch-card">
+                <div class="Add_Branch-fix-frame mt-4">
+                  <img src='https://s3-ap-southeast-1.amazonaws.com/wpimage.shopspotapp.com/wp-content/uploads/2016/07/15072043/re_IMG_9319.jpg' class="launches-img" width='100%' height='auto' alt='Baby Wallper'/>  
+                </div>
+                <div class="text-center mt-3">
+                  <h5>NEWCASTLE</h5>
+                </div>
+                <div class="text-center mt-3">
+                  <button type="button" class="btn Add_Branch-btn-secondary">STORE INFORMATION</button>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-6 mb-4">
+              <div class="Add_Branch-card">
+                <div class="Add_Branch-fix-frame mt-4">
+                  <img src='https://igx.4sqi.net/img/general/600x600/1358283489_xWtuKk38sApf4ArGF5DS6cfNXKFnP58_i5pv8j25QQM.jpg' class="launches-img" width='100%' height='auto' alt='Baby Wallper'/>  
+                </div>
+                <div class="text-center mt-3">
+                  <h5>GLASGOW</h5>
+                </div>
+                <div class="text-center mt-3">
+                  <button type="button" class="btn Add_Branch-btn-secondary">STORE INFORMATION</button>
+                </div>  
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- END SITE CONTENT -->
+
+
+      </div>
+    <!-- END CONTENT -->
+
+
+    <!-- START FOOTER -->
+    <?php include_once('inc/footer.php'); ?>
+    <!-- END FOOTER -->
+
+    <!-- START FOOTER SCRIPT -->
+    <?php include_once('inc/footer-script.php'); ?>
+    <!-- END FOOOTER SCRIPT -->
+
+
+  </body>
+</html>
